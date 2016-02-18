5e631e8669fe465e148ba1c747dd41ca8739a5ab
fullstack212 <keren.lerner@flatironschool.com>
2016-02-18 13:57:14 -0500

    finish serializers

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,3 +1,5 @@
 class MessageSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :topic_id, :content, :created_at, :updated_at 
+  belongs_to :user, embed: :ids, include: true
+  belongs_to :topic, embed: :ids, include: true
 end
--- app/serializers/topic_serializer.rb
+++ app/serializers/topic_serializer.rb
@@ -1,3 +1,5 @@
 class TopicSerializer < ActiveModel::Serializer
-  attributes :id, :building_id, :title, :description, :created_at, :updated_at 
+  attributes :id, :building_id, :title, :description, :created_at, :updated_at
+  belongs_to :building, embed: :ids, include: true
+  has_many :users, embed: :ids, include: true 
 end
