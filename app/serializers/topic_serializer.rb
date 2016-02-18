17fdb96980e3328cc8153bd93342476c55ea458c
fullstack212 <keren.lerner@flatironschool.com>
2016-02-18 14:42:40 -0500

    fixed associations for serializers

--- app/serializers/topic_serializer.rb
+++ app/serializers/topic_serializer.rb
@@ -1,5 +1,4 @@
 class TopicSerializer < ActiveModel::Serializer
   attributes :id, :building_id, :title, :description, :created_at, :updated_at
-  belongs_to :building, embed: :ids, include: true
   has_many :users, embed: :ids, include: true 
 end
