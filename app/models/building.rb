e63534ace533cafca842934ebaf228e2e801a915
leighsn <leighscherrer@gmail.com>
2016-02-20 11:12:47 -0500

    removed auth, modified building create actions

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,4 +2,5 @@ class Building < ActiveRecord::Base
   has_many :user_buildings
   has_many :users, through: :user_buildings
   has_many :topics
+  validates :name, uniqueness: { message: "It looks like that building might already exist."}
 end
